435c346174a41a60333887b9f011c39173aba678
Marcoandres <marcoandresha@gmail.com>
2024-11-22 15:00:33 -0500

    en el server url de app

--- server/app.js
+++ server/app.js
@@ -23,7 +23,7 @@ app.get('/', (req, res) => {
 })
 app.use(cors(
     {
-        origin: 'https://cinux-admin-ly9zs3a9z-marcoandres-mcs-projects.vercel.app/', //'https://fronted-production-43ca.up.railway.app'
+        origin: 'https://cinux-admin-ly9zs3a9z-marcoandres-mcs-projects.vercel.app/', 
         credentials: true
     }
 ))
